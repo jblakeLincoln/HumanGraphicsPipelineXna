87cbef6169acfd0cd580fe20133bdd19f31a91a4
jblakeLincoln <11210972@students.lincoln.ac.uk>
2014-07-11 12:27:24 +0100

    Removed superfluous references.

--- HumanGraphicsPipelineXna/HumanGraphicsPipelineXna/Scenes/TriangleScene.cs
+++ HumanGraphicsPipelineXna/HumanGraphicsPipelineXna/Scenes/TriangleScene.cs
@@ -1,13 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace HumanGraphicsPipelineXna
 {
@@ -175,11 +170,6 @@ namespace HumanGraphicsPipelineXna
                             pixelInBox.Y = j;
                             break;
                         }
-
-                        
-
-                        int y = 0;
-
                         count++;
                     }
                     if (breakNow)
